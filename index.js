672c5ff453de42edcd5dc636a976fceec7ffef08
Help-Desk23 <vian.sistemas@gmail.com>
2023-07-26 11:01:03 -0400

    New Update

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-{
-  "indexes": [],
-  "fieldOverrides": []
-}
+const express = require("express");
+const app = express();
+const { pool } = require("./src/database/db");
+const { getModels, addModels, updateModels, deleteModels} = require("./src/controllers/modelControlle");
+const routes = require("./src/v1/routes/modelRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./src/v1/swagger");
+
+
+app.use(express.json());
+
+// Ruta Principal
+app.get("/", (req, res) => {
+  res.end("PAGINA PRINCIAL HONDA!");
+});
+
+// Asociar el enrutador a la aplicación Express
+app.use("/", routes);
+
+// Arrancar el Servidor en el puerto especifico
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+  V1SwaggerDocs(app, port);
+});
