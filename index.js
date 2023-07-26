5deec7ba6e2be5f2dd0e296927be6997c1401dde
Help-Desk23 <vian.sistemas@gmail.com>
2023-07-26 10:32:13 -0400

    UpdateWensday

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-const express = require("express");
-const app = express();
-const { pool } = require("./src/database/db");
-const { getModels, addModels, updateModels, deleteModels} = require("./src/controllers/modelControlle");
-const routes = require("./src/v1/routes/modelRoutes");
-const { swaggerDocs: V1SwaggerDocs } = require("./src/v1/swagger");
-
-
-app.use(express.json());
-
-// Ruta Principal
-app.get("/", (req, res) => {
-  res.end("PAGINA PRINCIAL HONDA!");
-});
-
-// Asociar el enrutador a la aplicación Express
-app.use("/", routes);
-
-// Arrancar el Servidor en el puerto especifico
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-  V1SwaggerDocs(app, port);
-});
+{
+  "indexes": [],
+  "fieldOverrides": []
+}
